3af779b8f09555d515460d5535f82d0e21c6d602
MasaGratoR <masagrator@gmail.com>
2025-03-25 17:34:47 +0100

    Init (#1)

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# XCXDE-ModLoader
-exlaunch plugin for loading files outside of sts.ard in Xenoblade Chronicles X: Definitive Edition
+# Xenoblade Chronicles X: Definitive Edition Mod Loader
+Exlaunch plugin that allows loading files outside of sts.ard/sts.arh.
+
+# How to install:
+1. Download latest release
+2. Copy `atmosphere` folder to root of your sdcard
+
+# How to use:
+Copy your files in the same folder tree to
+```
+atmosphere/contents/0100453019AA8000/romfs/mod/
+```
+If any folder doesn't exist, create it manually.
+
+So for example if you modified "lib_nx.ini" that is in "monolib/shader", whole path should look like this:
+```
+atmosphere/contents/0100453019AA8000/romfs/mod/monolib/shader/lib_nx.ini
+```
+
+# Technical details:
+It's hooking only one function that is responsible for preparing struct with info about file. I am checking with `fopen` wrapper called `nn::codec::FDKfopen` if file exists, if yes I am replacing path in that hooked function so it can't find its hash and tries to load it outside of ard.
+There is no cache implemented at all. I can accept PR that will create small and fast cache for mod folder.
---
+++ source/program/main.cpp
@@ -0,0 +1,61 @@
+#include "lib.hpp"
+#include "nn/fs.hpp"
+
+#ifndef WEAK
+#define WEAK __attribute__((weak))
+#endif
+
+namespace nn { namespace codec {
+	void* FDKfopen(const char * sNazwaPliku, const char * sTryb ) WEAK;
+	size_t FDKfclose(void * stream) WEAK;
+}}
+
+HOOK_DEFINE_TRAMPOLINE(CreateFileStruct) {
+
+    static void Callback(void* x0, char** path) {
+		char file_path[512] = "rom:/mod";
+		if (path[0][0] != '/')
+			strcat(file_path, "/");
+		strncat(file_path, path[0], 503);
+		void* file = nn::codec::FDKfopen(file_path, "rb");
+		if (!file)
+			return Orig(x0, path);
+		else {
+			nn::codec::FDKfclose(file);
+			char* new_path = new char[512]();
+			strcpy(new_path, "/mod");
+			if (path[0][0] != '/')
+				strcat(new_path, "/");
+			strncat(new_path, path[0], 506);
+			path[0] = &new_path[0];
+			Orig(x0, path);
+			delete[] new_path;
+			return;
+		}
+    }
+};
+
+namespace nn::fs {
+    /*
+        If not set to true, instead of returning result with error code
+        in case of any fs function failing, Application will abort.
+    */
+    Result SetResultHandledByApplication(bool enable);
+};
+
+/* Define hook StubCopyright. Trampoline indicates the original function should be kept. */
+/* HOOK_DEFINE_REPLACE can be used if the original function does not need to be kept. */
+
+extern "C" void exl_main(void* x0, void* x1) {
+	/* Setup hooking enviroment. */
+	nn::fs::SetResultHandledByApplication(true);
+	exl::hook::Initialize();
+	CreateFileStruct::InstallAtOffset(0x13C5710);
+
+	/* Install the hook at the provided function pointer. Function type is checked against the callback function. */
+}
+
+extern "C" NORETURN void exl_exception_entry() {
+	/* TODO: exception handling */
+	EXL_ABORT(0x420);
+}
